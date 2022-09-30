6b5aac17f48d3df7fe23e4c2508427815aed28e9
JEDIBASIL <jedidiahbasil@gmail.com>
2022-09-30 02:18:18 +0100

    created an end point to add a user

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import { isUuid, uuid } from "uuidv4";
 const fs = require("fs");
 
 const AppRouter = express.Router();
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 const filePath = "user.json";
 const encoding = "UTF-8";
+
+ const  wiriteTofile  =(data,filePath)=>{
+    const jsonData = JSON.stringify(data)
+    fs.writeFile(filePath,jsonData,"utf8",(err)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log("file updated")
+    })
+}
 
 
 
@@ -20,9 +33,26 @@ app.get('/user/:id', (req , res)=>{
     const id = req.params.id;
     const jsonFormat = fs.readFile(filePath, encoding,(err, data)=>{
         const userData = JSON.parse(data);
+        if(id = null) res.send("User not found")
         res.send(userData.filter(user => user.id == id));
         res.send("found")
     });
+})
+
+app.post('/user', async (req , res)=>{
+    try{
+        let reqData = req.body;
+        const jsonFormat = fs.readFile(filePath, encoding,(err, data)=>{
+            let userData = JSON.parse(data);
+            const newUser = {id:uuid(),...reqData}
+            userData = { ...userData, newUser}
+            wiriteTofile(userData,filePath);
+            res.send(userData)
+        }) 
+    }catch(e){
+        res.send(e)
+    }
+   
 })
 
 const port = process.env.PORT || 8000;
