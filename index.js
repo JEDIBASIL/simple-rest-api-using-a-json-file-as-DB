85b44dadd2c31915f91428b303f8232639ca4978
JEDIBASIL <jedidiahbasil@gmail.com>
2022-10-01 10:36:38 +0100

    created an end point to update users

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import { isUuid, uuid } from "uuidv4";
+import { isUuid, uuid  } from "uuidv4";
 const fs = require("fs");
 
 const AppRouter = express.Router();
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.disable("trust proxy")
 const filePath = "user.json";
 const encoding = "UTF-8";
 
  const  wiriteTofile  =(data,filePath)=>{
-    const jsonData = JSON.stringify(data)
-    fs.writeFile(filePath,jsonData,"utf8",(err)=>{
+    fs.writeFile( filePath, JSON.stringify(data),"utf8",(err)=>{
         if(err){
             console.log(err)
         }
@@ -30,35 +30,56 @@ app.get('/users', (req , res)=>{
 })
 
 app.get('/user/:id', (req , res)=>{
-    const id = req.params.id;
+    let id = req.params.id;
     const jsonFormat = fs.readFile(filePath, encoding,(err, data)=>{
         const userData = JSON.parse(data);
-        if(id = null) res.send("User not found")
-        res.send(userData.filter(user => user.id == id));
-        res.send("found")
+        const user = userData.filter(user => user.id == id);
+        if(user.length == 0) {
+            id = 0;
+            res.send("user not found")
+        }else res.send(user[0]);
     });
 })
 
-app.post('/user', async (req , res)=>{
+app.post('/user', (req , res)=>{
     try{
         let reqData = req.body;
         const jsonFormat = fs.readFile(filePath, encoding,(err, data)=>{
             let userData = JSON.parse(data);
+            const user = userData.find(user=> user.email == reqData.email)
+            if(user)res.send("email already exist in the database");
+            else{
             const newUser = {id:uuid(),...reqData}
-            userData = { ...userData, newUser}
+            userData = [ ...userData, newUser]
             wiriteTofile(userData,filePath);
-            res.send(userData)
+            res.send("user added Successfully")
+        }
         }) 
     }catch(e){
         res.send(e)
     }
    
 })
+app.put('/user/:id', (req,res)=>{
+    const id = req.params.id;
+    const reqData = req.body;
+    const jsonFormat = fs.readFile(filePath, encoding,(err, data)=>{
+        let userData = JSON.parse(data);
+            const userFound = userData.find(user=> user.id == id)
+            if(userFound){
+                const userIndex = userData.findIndex((user => user.id == id));
+                const updatedDetails = {id:userData[userIndex].id, first_name: reqData.first_name, last_name: reqData.last_name, email:userData[userIndex].email, gender: reqData.gender, ip_address: reqData.ip_address }
+                userData[userIndex] = updatedDetails;
+                console.log(userData)
+            wiriteTofile(userData,filePath);
+            res.send("user updated successfully");
+            } else res.send("user not found")
+             
+        // 
+    });
+})
 
 const port = process.env.PORT || 8000;
 app.listen(port,()=>{
     console.log("now listening on port "+port)
 })
-
-
-
